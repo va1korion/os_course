e088c169bd35b9ffe68d947df440fde209b8d16d
va1korion <34692004+va1korion@users.noreply.github.com>
2020-09-16 21:18:20 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
-#include <zconf.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+#define KB 1024
+#define ALLOCSIZE (4 * KB)
+
 
 int main() {
+    char *point;
     int i = 0;
-    while (1){
-        fork();
-        printf("How many forks? %i", i);
+    while (1) {
+        point = mmap(NULL, ALLOCSIZE, PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        printf("%i \n", i++);
+
+        if (point == MAP_FAILED) {
+            printf("Hello there!");
+            break;
+        }
     }
+    return (0);
 }
